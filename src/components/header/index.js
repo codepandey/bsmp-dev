6d2ff70c4376bd07f4001796e649f96d17b2f3dc
codepanda <codepandey>
2022-12-30 22:16:24 +0530

    devops20

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -126,7 +126,7 @@ class Header extends Component {
                           style={{ overflow: "hidden", whiteSpace: "nowrap" }}
                         >
                           {" "}
-                          Welcome, User
+                          Welcomeee, User
                         </span>
                       </div>
                     </div>
